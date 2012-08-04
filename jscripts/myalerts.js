c97f2fb09e6968fccccc13749a23ca9317414710
Euan <euantorano@gmail.com>
2012-08-04 16:51:06 +0100

    Added myalerts.prototype.js - a prototype version of the javascript. Makes use of scriptaculous so we still have fancy effects on our dropdown.

--- jscripts/myalerts.js
+++ jscripts/myalerts.js
@@ -7,10 +7,18 @@ jQuery(document).ready(function($)
 		{
 			event.preventDefault();
 			var popup_id = $(this).attr('id') + '_popup';
-			console.log(popup_id);
 
 			$('#' + popup_id).attr('top', $(this).height() + 'px').slideToggle('slow');
 			return false;
 		}
 	}, '.myalerts_popup_hook');
+
+	if (typeof unreadAlerts !== 'undefined')
+	{
+    	if (unreadAlerts > 0)
+    	{
+    		document.title = document.title + ' (' + unreadAlerts + ')';
+    	}
+	}
+
 });
---
+++ jscripts/myalerts.prototype.js
@@ -0,0 +1,27 @@
+/*
+ *    Prototype version of the MyAlerts JS - just for LeeFish :)
+ *
+ *    @author Euan T. <euan@euantor.com>
+ */
+
+$$('.myalerts_popup_hook').each(function(elmt)
+{
+
+	elmt.observe('click', function(ev)
+	{
+		Event.stop(ev);
+		var popup_id = ev.target.identify() + '_popup';
+
+		Effect.toggle(popup_id, 'blind');
+
+		return false;
+	});
+});
+
+if (typeof unreadAlerts !== 'undefined')
+{
+	if (unreadAlerts > 0)
+	{
+		document.title = document.title + ' (' + unreadAlerts + ')';
+	}
+}
