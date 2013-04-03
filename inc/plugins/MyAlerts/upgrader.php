4f2b8622444b8829b637aba03f58da6c5df38a01
Euan T <euantorano@gmail.com>
2013-04-03 10:37:35 +0100

    Fixing upgrader.

--- inc/plugins/MyAlerts/upgrader.php
+++ inc/plugins/MyAlerts/upgrader.php
@@ -10,29 +10,50 @@ function myalerts_upgrader_run($currentVersion = '1.04', $oldVersion = '1.00')
 		global $db;
 
 		if (!$db->table_exists('alert_settings')) {
-	        $collation = $db->build_create_table_collation();
-	        $db->write_query("CREATE TABLE ".TABLE_PREFIX."alert_settings(
-	            id INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	            code VARCHAR(75) NOT NULL
-	            ) ENGINE=MyISAM{$collation};");
-	    }
+			$collation = $db->build_create_table_collation();
+			$db->write_query("CREATE TABLE ".TABLE_PREFIX."alert_settings(
+				id INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+				code VARCHAR(75) NOT NULL
+				) ENGINE=MyISAM{$collation};");
+		}
 
-	    if (!$db->table_exists('alert_setting_values')) {
-	        $collation = $db->build_create_table_collation();
-	        $db->write_query("CREATE TABLE ".TABLE_PREFIX."alert_setting_values(
-	            id INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	            user_id INT(10) NOT NULL,
-	            setting_id INT(10) NOT NULL,
-	            value INT(1) NOT NULL DEFAULT '1'
-	            ) ENGINE=MyISAM{$collation};");
-	    }
+		if (!$db->table_exists('alert_setting_values')) {
+			$collation = $db->build_create_table_collation();
+			$db->write_query("CREATE TABLE ".TABLE_PREFIX."alert_setting_values(
+				id INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+				user_id INT(10) NOT NULL,
+				setting_id INT(10) NOT NULL,
+				value INT(1) NOT NULL DEFAULT '1'
+				) ENGINE=MyISAM{$collation};");
+		}
 
-	    if ($db->field_exists('myalerts_settings', 'users')) {
-	    	$db->drop_column('users', 'myalerts_settings');
-	    }
+		if ($db->field_exists('myalerts_settings', 'users')) {
+			$db->drop_column('users', 'myalerts_settings');
+		}
 
-	    if ($db->field_exists('forced', 'alerts')) {
-	    	$db->add_column('alerts', 'forced', "INT(1) NOT NULL DEFAULT '0'");
-	    }
+		if ($db->field_exists('forced', 'alerts')) {
+			$db->add_column('alerts', 'forced', "INT(1) NOT NULL DEFAULT '0'");
+		}
+
+		// Settings
+		$insertArray = array(
+			0 => array(
+				'code' => 'rep',
+			),
+			1 => array(
+				'code' => 'pm',
+			),
+			2 => array(
+				'code' => 'buddylist',
+			),
+			3 => array(
+				'code' => 'quoted',
+			),
+			4 => array(
+				'code' => 'post_threadauthor',
+			),
+		);
+
+		$db->insert_query_multiple('alert_settings', $insertArray);
 	}
 }
