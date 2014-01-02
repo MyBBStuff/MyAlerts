08f2ba2674206d114fe075f62d47c094958a1a03
Alfredo Ramos <AlfredoRamos@users.noreply.github.com>
2014-01-01 18:12:54 -0600

    Create espanol.php

---
+++ inc/languages/espanol/espanol.php
@@ -0,0 +1,71 @@
+<?php
+$l['myalerts'] = 'MyAlerts';
+
+$l['myalerts_page_title'] = 'Alertas Recientes';
+$l['myalerts_page_getnew'] = 'Buscar nuevas alertas';
+
+$l['myalerts_alerts'] = 'Alertas';
+
+$l['myalerts_settings_page_title'] = 'Configuración de Alertas';
+
+$l['myalerts_online_location_listing'] = 'Viendo Alertas';
+
+$l['myalerts_unread_title'] = 'Alertas sin leer';
+$l['myalerts_view_all'] = 'Ver todas las Alertas';
+
+$l['myalerts_loading'] = 'Cargando...';
+$l['myalerts_no_alerts'] = 'No hay Alertas para mostrar';
+$l['myalerts_no_new_alerts'] = 'No tienes nuevas Alertas.';
+$l['myalerts_rep'] = '{1} modificó <a href="reputation.php?uid={2}">tu reputación</a>. ({3})';
+$l['myalerts_pm'] = '{1} te envió un nuevo mensaje privado titulado "{2}". ({3})';
+$l['myalerts_buddylist'] = '{1} te agregó a su lista de amigos. ({2})';
+$l['myalerts_quoted'] = '{1} te citó en <a href="{2}">{3}</a>. ({4})';
+$l['myalerts_post_threadauthor'] = '{1} respondió a tu tema "<a href="{2}">{3}</a>". Es posible que haya más respuestas. ({4})';
+
+$l['myalerts_setting_rep'] = 'Recibir Alerta por cambios de Reputación?';
+$l['myalerts_setting_pm'] = 'Recibir Alerta al recibir un nuevo Mensaje Privado (MP)?';
+$l['myalerts_setting_buddylist'] = 'Recibir Alerta al ser añadido en una lista de amigos';
+$l['myalerts_setting_quoted'] = 'Recibir Alerta al ser citado en un tema';
+$l['myalerts_setting_post_threadauthor'] = 'Recibir Alerta cuando alguien responda tu tema';
+$l['myalerts_settings_save'] = 'Guardar Configuración';
+$l['myalerts_settings_updated'] = 'La configuración de las Alertas se ha actualizado exitosamente. Redirigiendo a la página de configuración.';
+$l['myalerts_settings_updated_title'] = 'Configuración de las Alertas actualizada.';
+
+$l['myalerts_delete_deleted'] = 'Alerta eliminada exitosamente.';
+$l['myalerts_delete_error'] = 'La Alerta no puede ser eliminada en este momento.';
+$l['myalerts_delete_read_confirm'] = '¿En verdad deseas eliminar todas las Alertas leídas? No se puede deshacer!';
+$l['myalerts_delete_all_confirm'] = '¿En verdad deseas eliminar todas las Alertas? No se puede deshacer!';
+$l['myalerts_delete_mass_deleted'] = 'Alertas Eliminadas';
+$l['myalerts_delete_mass_error'] = 'Error al eliminar Alertas';
+$l['myalerts_delete_mass_error_more'] = 'Lo siendo, algo salió mal mientras se eliminaban tus Alertas. Por favor, inténtalo de nuevo.';
+$l['myalerts_delete_all_read'] = 'Todas las Alertas leídas han sido eliminadas exitosamente.';
+$l['myalerts_delete_all'] = 'Todas las alertas han sido eliminadas exitosamente.';
+
+$l['myalerts_usercp_nav'] = 'Alertas';
+$l['myalerts_usercp_nav_alerts'] = 'Ver Alertas';
+$l['myalerts_usercp_nav_settings'] = 'Configuración de Alertas';
+$l['myalerts_usercp_nav_delete_read'] = 'Eliminar Alertas antiguas';
+$l['myalerts_usercp_nav_delete_all'] = 'Eliminar todas las Alertas';
+
+$l['myalerts_help_alert_types'] = 'Tipos de Alertas';
+$l['myalerts_help_alert_types_rep'] = '<strong>Reputación</strong>
+<p>
+	Esta alerta es recibida cuando alguien modifica tu reputación.
+</p>';
+$l['myalerts_help_alert_types_pm'] = '<strong>Mensaje Privado</strong>
+<p>
+	Esta alerta es recibida cuando alguien te envia un nuevo Mensaje Privado en el sitio. Éste te mostrará el remitente y el título del mismo, al igual que una noticia en la parte superio del sitio.
+</p>';
+$l['myalerts_help_alert_types_buddylist'] = '<strong>Agregado a Lista de Amigos</strong>
+<p>
+	Esta alerta es recibida cuando eres agregado a la Lista de Amigos de otro miembro en el sitio.
+</p>';
+$l['myalerts_help_alert_types_quoted'] = '<strong>Citado en un tema</strong>
+<p>
+	Esta alerta es recibida cuando eres citado dentro de un tema por otro miembro en cualquier lugar en el sitio.
+</p>';
+$l['myalerts_help_alert_types_post_threadauthor'] = '<strong>Nueva respuesta a tu tema</strong>
+<p>
+	Esta alerta es recibida cuando tu tema recibe una nueva respuesta por otro miembro del sitio. Sólo se mostrara la primer nueva respuesta, siendo las respuestas posteriores a ella ignoradas para evitar alertas duplicadas.
+</p>';
+?>
