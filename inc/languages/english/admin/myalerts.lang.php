d2f41b7e27cf801ebaab7f239e185c6ec0822546
Eldenroot <Eldenroot@gmail.com>
2016-02-21 13:33:36 +0100

    Some lang changes

--- inc/languages/english/admin/myalerts.lang.php
+++ inc/languages/english/admin/myalerts.lang.php
@@ -9,15 +9,15 @@ $l['setting_myalerts_perpage_desc'] = "How many alerts do you wish to display on
 $l['setting_myalerts_dropdown_limit'] = "Number of alerts to show in dropdown";
 $l['setting_myalerts_dropdown_limit_desc'] = "How many alerts do you wish to display in the global alerts dropdown? (default is 5)";
 $l['setting_myalerts_autorefresh'] = "MyAlerts page AJAX autorefresh";
-$l['setting_myalerts_autorefresh_desc'] = "How often (in seconds) do you wish the MyAlerts page to refresh the alerts listing via AJAX? (0 for no autorefresh)";
+$l['setting_myalerts_autorefresh_desc'] = "How often (in seconds) do you wish the MyAlerts page in User control panel to refresh the alerts listing via AJAX? (0 for no autorefresh)";
 $l['setting_myalerts_avatar_size'] = "Avatar Dimensions";
-$l['setting_myalerts_avatar_size_desc'] = "The dimensions to use when displaying avatars in alert listings. In the form x|y. Example: 64|64.";
+$l['setting_myalerts_avatar_size_desc'] = "The dimensions to use when displaying avatars in alert listings. (In the form width|height. Example: 64|64.)";
 
 $l['myalerts_task_cleanup_ran'] = 'Read alerts over a week old were deleted successfully!';
 $l['myalerts_task_cleanup_error'] = 'Something went wrong while cleaning up the alerts...';
 
 $l['myalerts_task_title'] = 'MyAlerts Cleanup';
-$l['myalerts_task_description'] = 'A task to cleanup old read alerts. THis is required as otherwise the alerts table could swell to massive sizes.';
+$l['myalerts_task_description'] = 'A task to cleanup old read alerts. This is required as otherwise the alerts table could swell to massive sizes.';
 
 $l['myalerts_alert_types'] = 'Alert Types';
 $l['myalerts_can_manage_alert_types'] = 'Can manage alert types?';
