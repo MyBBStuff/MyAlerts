f7cdd3d69dae0e9c2fe660dbdc9c0477a7f9858b
Laird Shaw <lairdshaw77@gmail.com>
2018-09-14 16:42:13 +1000

    Fixes #230: Return URL missing query parameters when clicking "Delete all alerts" in modal

--- inc/plugins/myalerts.php
+++ inc/plugins/myalerts.php
@@ -810,7 +810,7 @@ function myalerts_global_intermediate()
 			$newAlertsIndicator = 'alerts--new';
 		}
 
-		$myalerts_return_link = htmlspecialchars_uni(myalerts_get_current_url());
+		$myalerts_return_link = htmlspecialchars_uni(urlencode(myalerts_get_current_url()));
 
 		$myalerts_headericon = eval($templates->render('myalerts_headericon'));
 
