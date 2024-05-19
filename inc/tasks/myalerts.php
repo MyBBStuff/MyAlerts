d9db885226c241793ecba68e41b157f1d17cb170
Laird <lairdshaw77@gmail.com>
2024-05-19 17:35:04 +1000

    Fix #364: The language strings for the task are misleading/missing
    Resolves #364

--- inc/tasks/myalerts.php
+++ inc/tasks/myalerts.php
@@ -15,17 +15,19 @@ if (!defined('IN_MYBB')) {
 function task_myalerts($task)
 {
 	global $db, $lang;
-	
+
+	$read_days = 90; // 90 days for read alerts
+	$unread_days = 120; // 120 days for unread alerts 
 	// Read/unread alerts older than X seconds will be deleted to save some space in DB
-	$read_time = 90*24*60*60; // 90 days for read alerts (formula: days X hours X minutes X seconds)
-	$unread_time = 120*24*60*60; // 120 days for unread alerts (formula: days X hours X minutes X seconds)
+	$read_time = $read_days*24*60*60; // Formula: days X hours X minutes X seconds
+	$unread_time = $unread_days*24*60*60; //Formula: days X hours X minutes X seconds
 	
 	if (!isset($lang->myalerts)) {
 		$lang->load('myalerts');
 	}
 
 	if ($db->delete_query('alerts', '(dateline <= \''.date('Y-m-d H:i:s', TIME_NOW-$unread_time).'\' AND unread = 1) OR (dateline <= \''.date('Y-m-d H:i:s', TIME_NOW-$read_time).'\' AND unread = 0)')) {
-		add_task_log($task, $lang->myalerts_task_cleanup_ran);
+		add_task_log($task, $lang->sprintf($lang->myalerts_task_cleanup_ran, $read_days, $unread_days));
 	} else {
 		add_task_log($task, $lang->myalerts_task_cleanup_error);
 	}
